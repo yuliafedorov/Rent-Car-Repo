fcf17f27995e01073c5de3299b88b1e3aaec5356
yuliafedorov <fedorovayulia150@gmail.com>
2021-10-19 00:16:23 +0300

    Add LWC Component on Sales Manager Page

---
+++ force-app/main/default/lwc/openDeals/openDeals.js
@@ -0,0 +1,29 @@
+import { LightningElement, api, wire, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getContactListyy from '@salesforce/apex/OpenDealsController.returnOpenDeals';
+
+
+const columns = [
+    { label: 'Name', fieldName: 'Name' },
+    { label: 'Car', fieldName: 'Car_Name__c' },
+    { label: "Start Date", fieldName: "Start_Date__c"},
+    { label: "End Date", fieldName: "End_Date__c"}
+];
+export default class OpenDeals extends LightningElement {
+    @api recordId;
+    error;
+    columns = columns;
+
+     @wire(getContactListyy, { salesManagerId: '$recordId' })
+    contacts;
+
+    showToast() {
+        const event = new ShowToastEvent({
+            title: 'Warning',
+            message: 'Manager Sync is not yet configured',
+            variant: 'warning'
+        });
+        this.dispatchEvent(event);
+    }
+
+}
