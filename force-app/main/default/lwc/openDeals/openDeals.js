60f1feafe83503dd0d917f9091eecc73d6e6472c
yuliafedorov <fedorovayulia150@gmail.com>
2021-11-01 12:45:47 +0200

    Push Sales Manager to the external system

--- force-app/main/default/lwc/openDeals/openDeals.js
+++ force-app/main/default/lwc/openDeals/openDeals.js
@@ -1,14 +1,32 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import returnOpenDeals from '@salesforce/apex/OpenDealsController.returnOpenDeals';
-//import pushSalesManager from '@salesforce/apex/OpenDealsController.pushSalesManager';
+import pushSalesManager from '@salesforce/apex/OpenDealsController.pushSalesManager';
 
 
 const columns = [
     { label: 'Name', fieldName: 'Name' },
     { label: 'Car', fieldName: 'Car_Name__c' },
-    { label: "Start Date", fieldName: "Start_Date__c"},
-    { label: "End Date", fieldName: "End_Date__c"}
+    { label: "Start Date", fieldName: "Start_Date__c",
+        type: "date",
+        typeAttributes:{
+            year: "numeric",
+            month: "long",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit"
+        }
+    },
+    { label: "End Date", fieldName: "End_Date__c",
+        type: "date",
+        typeAttributes:{
+            year: "numeric",
+            month: "long",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit"
+        }
+    }
 ];
 export default class OpenDeals extends LightningElement {
     @api recordId;
@@ -17,28 +35,36 @@ export default class OpenDeals extends LightningElement {
     @wire(returnOpenDeals, { salesManagerId: '$recordId' })
     deals;
 
-    showToast() {
-        const event = new ShowToastEvent({
-            title: 'Warning',
-            message: 'Manager Sync is not yet configured',
-            variant: 'warning'
-        });
-        this.dispatchEvent(event);
+    pushSalesManager (){
+        pushSalesManager({
+            salesManagerId: this.recordId
+        })
+        .then(result => {
+            const event = new ShowToastEvent({
+                title: "Sucess!",
+                message: "System manager is synchronized.",
+                variant: "success",
+                mode: "pester",
+                });
+                this.dispatchEvent(event);
+        })
+        .catch(error => {
+            const event = new ShowToastEvent({
+                title: "Error!",
+                message: "System manager is not synchronized.",
+                variant: "error",
+                mode: "pester",
+                });
+                this.dispatchEvent(event);
+        })
     }
 
-    // pushSalesManager (){
-    //     pushSalesManager({
-    //         salesManagerId: this.recordId
-    //     })
-    //     .then(result => {
-    //         if(result){
-    //             console.log(result);
-    //             console.log('+++');
-    //         }
-    //     })
-    //     .catch(error => {
-    //         console.log('Error: ', error);
-    //         console.log('------');
-    //     })
+    // showToast() {
+    //     const event = new ShowToastEvent({
+    //         title: 'Warning',
+    //         message: 'Manager Sync is not yet configured',
+    //         variant: 'warning'
+    //     });
+    //     this.dispatchEvent(event);
     // }
 }
